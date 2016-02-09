28f239609214b7c34915c1c9cfb7b453c406bdaa
Hristo Deshev <hristo@deshev.com>
2016-02-09 16:44:48 +0200

    Update ng-sample build hooks

--- ng-sample/hooks/before-prepare/10-copy-renderer.js
+++ ng-sample/hooks/before-prepare/10-copy-renderer.js
@@ -1,7 +1,12 @@
 var path = require('path');
 var shelljs = require('shelljs');
 
-module.exports = function() {
-    shelljs.cp('-Rf', '../src/nativescript-angular', 'app');
-    shelljs.cp('-Rf', '../src/global.d.ts', 'app');
+module.exports = function ($logger, $projectData, $usbLiveSyncService) {
+    var projectDir = path.join(__dirname, '..', '..');
+    var appDir = path.join(projectDir, 'app');
+    var srcDir = path.join(projectDir, '..', 'src');
+    if (!$usbLiveSyncService.isInitialized) {
+        shelljs.cp('-Rf', path.join(srcDir, 'nativescript-angular'), appDir);
+        shelljs.cp('-Rf', path.join(srcDir, 'global.d.ts'), appDir);
+    }
 }
--- ng-sample/hooks/before-prepare/30-remove-stale-widgets-jar.js
+++ ng-sample/hooks/before-prepare/30-remove-stale-widgets-jar.js
@@ -2,12 +2,18 @@ var path = require('path');
 var shelljs = require('shelljs');
 
 module.exports = function ($logger, $projectData, $usbLiveSyncService) {
+    var projectDir = path.join(__dirname, '..', '..');
 	if (!$usbLiveSyncService.isInitialized) {
         // Delete stale widgets.jar brought by old Android platforms.
         // TODO: Remove this after version 1.6.0 of the Android runtime gets released
-        var jars = shelljs.find('platforms/android').filter(function(fileName){
-            return fileName.match(/widgets.jar$/);
-        });
+        var androidDir = path.join(projectDir, 'platforms/android');
+		var jars = [];
+        if (shelljs.test('-d', androidDir)) {
+            jars = shelljs.find(androidDir).filter(function(fileName){
+                return fileName.match(/widgets.jar$/);
+            });
+        }
+
         jars.forEach(function(jarFile) {
             console.log('Deleting stale jar file: ' + jarFile);
             shelljs.rm(jarFile);
