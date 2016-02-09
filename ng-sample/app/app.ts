b84e3a032369f8a905b8a7071f699032f2a9b134
Hristo Deshev <hristo@deshev.com>
2016-02-09 16:45:07 +0200

    Clean comments from ng-sample/app/app.ts

--- ng-sample/app/app.ts
+++ ng-sample/app/app.ts
@@ -1,21 +1,14 @@
-//import "globals";
-// import "./modules";
-//global.registerModule("./main-page", function () { return require("./main-page"); });
-
-//import * as profiling from "./profiling";
-//profiling.start('application-start');
-
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
 import { nativeScriptBootstrap } from "./nativescript-angular/application";
 
-//import {RendererTest} from './examples/renderer-test';
-//import {Benchmark} from './performance/benchmark';
-//import {ListTest} from './examples/list/list-test';
+import {RendererTest} from './examples/renderer-test';
+import {Benchmark} from './performance/benchmark';
+import {ListTest} from './examples/list/list-test';
 import {ListTestAsync} from "./examples/list/list-test-async";
-// import {ImageTest} from "./examples/image/image-test";
+import {ImageTest} from "./examples/image/image-test";
 
 //nativeScriptBootstrap(RendererTest);
-//nativeScriptBootstrap(Benchmark);
+nativeScriptBootstrap(Benchmark);
 //nativeScriptBootstrap(ListTest);
-nativeScriptBootstrap(ListTestAsync);
+//nativeScriptBootstrap(ListTestAsync);
 // nativeScriptBootstrap(ImageTest);
