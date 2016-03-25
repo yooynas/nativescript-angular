ada886f23b3102a91a9cb5bee82b4f080ae00ae1
Hristo Deshev <hristo@deshev.com>
2016-03-25 19:14:09 +0200

    WIP webpack - first successful aliased module bundle
    Requires a change in tns-core-modules imports

--- ng-sample/webpack.config.js
+++ ng-sample/webpack.config.js
@@ -1,9 +1,11 @@
 var path = require("path");
 var webpack = require("webpack");
+var tnsCoreModulesAliases = require("./module-aliases").tnsCoreModulesAliases;
 
-
-console.log('PLATFORM: ' + process.env.PLATFORM);
-console.log('PLATFORM_DIR: ' + process.env.PLATFORM_DIR);
+var platform = process.env.PLATFORM;
+var platformOutDir = process.env.PLATFORM_DIR;
+console.log('PLATFORM: ' + platform);
+console.log('PLATFORM_DIR: ' + platformOutDir);
 
 module.exports = {
     context: "./app",
@@ -18,6 +20,8 @@ module.exports = {
     },
     externals: [
         function(context, request, callback) {
+			//console.log("external: " + context + ", request: " + request);
+			//console.log(callback.toString());
             callback();
             //if (/browserify|crypto/.test(request)) {
                 //return callback(null, "var {}");
@@ -32,12 +36,13 @@ module.exports = {
         modulesDirectories: [
             "../node_modules",
         ],
-        alias: {
-            "ui/text-view": "tns-core-modules/ui/text-view/text-view.android.js",
-            "ui/text-base": "tns-core-modules/ui/text-base/text-base.android.js",
-            "ui/editable-text-base": "tns-core-modules/ui/editable-text-base/editable-text-base.android.js",
-            "ui/core/proxy": "tns-core-modules/ui/core/proxy",
-        }
+		alias: tnsCoreModulesAliases(platform),
+        //alias: {
+            //"ui/text-view": "tns-core-modules/ui/text-view/text-view.android.js",
+            //"ui/text-base": "tns-core-modules/ui/text-base/text-base.android.js",
+            //"ui/editable-text-base": "tns-core-modules/ui/editable-text-base/editable-text-base.android.js",
+            //"ui/core/proxy": "tns-core-modules/ui/core/proxy",
+        //}
     },
     module: {
         loaders: [
