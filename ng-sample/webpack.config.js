c30189d508deb72cec7a80c3c37012752f01e630
Hristo Deshev <hristo@deshev.com>
2016-03-29 17:50:54 +0300

    WIP use nativescript-dev-webpack package (npm-link-ed)

--- ng-sample/webpack.config.js
+++ ng-sample/webpack.config.js
@@ -1,38 +1,5 @@
-var path = require("path");
-var webpack = require("webpack");
-var tnsCoreModulesAliases = require("./module-aliases").tnsCoreModulesAliases;
+var bundler = require("nativescript-dev-webpack");
 
-var platform = process.env.PLATFORM;
-var platformOutDir = process.env.PLATFORM_DIR;
-console.log('PLATFORM: ' + platform);
-console.log('PLATFORM_DIR: ' + platformOutDir);
-
-module.exports = {
-    context: "./app",
-    entry: {
-        app: "./app",
-    },
-    output: {
-        pathinfo: true,
-        libraryTarget: "commonjs2",
-        filename: path.join(__dirname, platformOutDir, "bundle.js"),
-    },
-    resolve: {
-        extensions: ["", ".js"],
-        packageMains: ["main"],
-        modulesDirectories: [
-            "../node_modules",
-        ],
-        alias: tnsCoreModulesAliases(platform),
-    },
-    module: {
-        loaders: [
-        ]
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            global: 'global',
-            __dirname: '__dirname'
-        }),
-    ]
-};
+module.exports = bundler.getConfig({
+    // TODO: add project-specific webpack settings here...
+});
